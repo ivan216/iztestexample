23b9de2a95d2b51bb93a5c238fb7d00cdbd6fefe
ivan216 <64449776+ivan216@users.noreply.github.com>
2024-08-16 23:21:06 +0800

    Add files via upload

--- 3y_h_/3y_h_.py
+++ 3y_h_/3y_h_.py
@@ -1,9 +1,8 @@
 from rpze.basic.inject import InjectedGame
 from rpze.iztest.iztest import IzTest
+from rpze.rp_extend import Controller
 from rpze.iztest.operations import place
 from rpze.flow.utils import until
-from rpze.rp_extend import Controller
-from random import randint
 
 def fun(ctler: Controller):
     iz_test = IzTest(ctler).init_by_str('''
@@ -18,21 +17,18 @@ def fun(ctler: Controller):
         0  
         3-6''')
     
-    ts_fail_count = 0 #没吃掉三线的次数
+    tp = None   #用来存三线射手
+    tp_fail_count = 0 #没吃掉三线的次数
     _125_count = 0 #补铁桶次数
     _75_count = 0 #补75的次数
 
-###结论：橄榄176，铁桶174，障（补障）杆（补杆）出第二黄油直接补障 173
-
     @iz_test.flow_factory.add_flow()
     async def place_zombie(_):
-        nonlocal _75_count,_125_count
-        zlist = iz_test.game_board.zombie_list
-        plist = iz_test.ground
-        tt = zlist[0]
-        ts = plist["3-1"]
-        y = plist["3-2"]
-        s = plist["3-4"]
+        nonlocal _75_count,_125_count,tp
+        tt = iz_test.game_board.zombie_list[0]
+        tp = iz_test.ground["3-1"]
+        y = iz_test.ground["3-2"]
+        s = iz_test.ground["3-4"]
 
         while not s.is_dead:
             await (until(lambda _:s.is_dead) | until(lambda _:tt.hp <= 89)) #伞先死还是桶先死
@@ -44,7 +40,7 @@ def fun(ctler: Controller):
             cg = place("cg 3-6")     #等到铁桶快死或者比较近补杆
             _75_count += 1
             await until(lambda _:cg.hp <= 210) 
-            if ts.hp >=300 :     #杆也过不去
+            if tp.hp >=300 :     #杆也过不去
                 place("cg 3-6") #补第二杆
                 _75_count += 1
         else :
@@ -53,7 +49,7 @@ def fun(ctler: Controller):
                 cg = place("cg 3-6")        #铁桶已经下到一定血量，且比较靠后，那么直接补杆 280
                 _75_count += 1
                 await until(lambda _:cg.hp <= 210) 
-                if ts.hp >=300:     #杆也过不去
+                if tp.hp >=300:     #杆也过不去
                     place("cg 3-6") #补第二杆
                     _75_count += 1
             else :
@@ -62,36 +58,34 @@ def fun(ctler: Controller):
                     cg = place("cg 3-6")     #补杆
                     _75_count += 1
                     await until(lambda _:cg.hp <= 210) 
-                    if ts.hp >=300:     #杆也过不去
+                    if tp.hp >=300:     #杆也过不去
                         place("cg 3-6") #补第二杆
                         _75_count += 1
                 elif tt.hp <= 210:    #啃完玉米但铁桶已经比较残
                     lz = place("lz 3-6")     #补障
                     _75_count += 1
                     await until(lambda _:lz.hp <= 190) 
-                    if (ts.hp >=300) & (lz.int_x >= 120) :    #障也过不去
+                    if (tp.hp >=300) & (lz.int_x >= 120) :    #障也过不去
                         place("lz 3-6")     #补第二障
                         _75_count += 1
                 else:
                     await until(lambda _:tt.hp <= 190) 
-                    if (ts.hp >=300) & (tt.int_x >= 120) :  #看是否需要补障
+                    if (tp.hp >=300) & (tt.int_x >= 120) :  #看是否需要补障
                         lz = place("lz 3-6")     #补障
                         _75_count += 1
                         await until(lambda _:lz.hp <= 190) 
-                        if (ts.hp >=300) & (lz.int_x >= 120) :    #障也过不去
+                        if (tp.hp >=300) & (lz.int_x >= 120) :    #障也过不去
                             place("lz 3-6")     #补第二障
                             _75_count += 1
         
     @iz_test.on_game_end()
     def end_callback(result: bool):
-        if not result:
-            nonlocal ts_fail_count
-            plant_list = iz_test.ground
-            if plant_list["3-1"] is not None:
-                ts_fail_count += 1
+        nonlocal tp_fail_count,tp
+        if not tp.is_dead :
+            tp_fail_count += 1
     
-    iz_test.start_test(jump_frame=0, speed_rate=10)
-    print("漏三线或失败:",ts_fail_count)
+    iz_test.start_test(jump_frame=1, speed_rate=10)
+    print("漏三线:",tp_fail_count)
     print("补桶:",_125_count)
     print("补75:",_75_count)
 
--- 3y_h_/3y_h_2.py
+++ 3y_h_/3y_h_2.py
@@ -2,14 +2,14 @@ from rpze.basic.inject import InjectedGame
 from rpze.iztest.iztest import IzTest
 from rpze.iztest.operations import place
 from rpze.flow.flow import FlowManager
-from rpze.flow.utils import AwaitableCondFunc, VariablePool ,until ,delay
+from rpze.flow.utils import AwaitableCondFunc, VariablePool ,until
 from rpze.structs.plant import Plant, PlantStatus
 from rpze.rp_extend import Controller
 from random import randint
 
-def count_butter(plant: Plant, n:int = 1) -> AwaitableCondFunc:         #通过状态数黄油数量
+def count_butter(plant: Plant, n:int = 1) -> AwaitableCondFunc:
     def _cond_func(fm: FlowManager,v = VariablePool( projs = 0, try_to_shoot_time=None )):
-        if plant.generate_cd == 1:                                      # 下一帧开打
+        if plant.generate_cd == 1:
             v.try_to_shoot_time = fm.time + 1
         if v.try_to_shoot_time == fm.time :
             if plant.status is PlantStatus.kernelpult_launch_butter :
@@ -39,13 +39,12 @@ def fun(ctler: Controller):
     @iz_test.flow_factory.add_flow()
     async def place_zombie(_):
         nonlocal _75_count
-        zlist = iz_test.game_board.zombie_list
-        lz = zlist[0]
-        plist = iz_test.ground
-        ts = plist["3-1"]
-        y = plist["3-2"]
-        s = plist["3-4"]
-        await (count_butter(y,2).after(115) | until(lambda _:s.is_dead) | until(lambda _:lz.hp < 90))
+        lz = iz_test.game_board.zombie_list[0]
+        tp = iz_test.ground["3-1"]
+        y = iz_test.ground["3-2"]
+        s = iz_test.ground["3-4"]
+
+        await (count_butter(y,2).after(142) | until(lambda _:s.is_dead) | until(lambda _:lz.hp < 90))
         if not s.is_dead:
             place("lz 3-6")
             _75_count += 1
@@ -53,11 +52,11 @@ def fun(ctler: Controller):
         cg = place("cg 3-6")
         _75_count += 1
         await until(lambda _:cg.hp <= 210) 
-        if ts.hp >=300:     #杆也过不去
+        if tp.hp >=300:     #杆也过不去
             place("cg 3-6") #补第二杆
             _75_count += 1
 
-    iz_test.start_test(jump_frame=0, speed_rate=5)
+    iz_test.start_test(jump_frame=1, speed_rate=5)
     print("补75:",_75_count)
 
 with InjectedGame(r"D:\pvz\Plants vs. Zombies 1.0.0.1051 EN\PlantsVsZombies.exe") as game:
