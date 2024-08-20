0efd0ce764bd0f712025a92f46520721d5f4f694
ivan216 <64449776+ivan216@users.noreply.github.com>
2024-08-20 23:02:10 +0800

    Add files via upload

---
+++ chuanci/dpppc.py
@@ -0,0 +1,60 @@
+from rpze.basic.inject import InjectedGame
+from rpze.iztest.iztest import IzTest
+from rpze.rp_extend import Controller
+from rpze.flow.utils import until, delay
+from rpze.iztest.operations import place ,repeat
+from rpze.iztest.cond_funcs import until_plant_die
+from rpze.flow.flow import FlowManager
+from rpze.flow.utils import AwaitableCondFunc, VariablePool
+from rpze.structs.plant import Plant
+from random import randint
+
+def until_plant_n_shoot(plant: Plant, n:int = 1) -> AwaitableCondFunc:
+    def _cond_func(fm: FlowManager,
+                v=VariablePool(try_to_shoot_time=None, shots = 0)):
+        if plant.generate_cd == 1:  # 下一帧开打
+            v.try_to_shoot_time = fm.time + 1
+        if v.try_to_shoot_time == fm.time and plant.launch_cd != 0:  # 在攻击时
+            v.shots += 1
+        if v.try_to_shoot_time == fm.time and plant.launch_cd == 0: #不再攻击时，计数清零
+            v.shots = 0
+        if v.shots == n:
+            return True
+        return False
+    return AwaitableCondFunc(_cond_func)
+
+def fun(ctler: Controller):
+    iz_test = IzTest(ctler).init_by_str('''
+        5000 -1
+        3-0
+        .....
+        .....
+        dpppc
+        .....
+        .....
+        cg 
+        0  
+        3-6 ''') # 花费200
+    
+    _75_count = 0
+    
+    @iz_test.flow_factory.add_flow()
+    async def place_zombie(fm: FlowManager):
+        nonlocal _75_count
+        c = iz_test.ground["3-5"]
+        d = iz_test.ground["3-1"]
+
+        await until_plant_n_shoot(d).after(40)
+        place("xg 3-6")
+        await until_plant_die(c)
+        await until_plant_n_shoot(d).after(0)
+        cg = place("cg 3-6")
+        await until(lambda _:cg.hp < 170)
+        place("cg 3-6")
+        _75_count += 1
+
+    iz_test.start_test(jump_frame=1, speed_rate=2)
+    print(_75_count)
+
+with InjectedGame(r"D:\pvz\Plants vs. Zombies 1.0.0.1051 EN\PlantsVsZombies.exe") as game:
+    fun(game.controller)
