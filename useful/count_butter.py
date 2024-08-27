11b4a4141933384275d13ae0a4c18411d35732b9
ivan216 <64449776+ivan216@users.noreply.github.com>
2024-08-27 12:24:29 +0800

    Add files via upload

--- useful/count_butter.py
+++ useful/count_butter.py
@@ -7,15 +7,19 @@ from rpze.structs.plant import Plant , PlantStatus
 from rpze.structs.projectile import ProjectileType
 from rpze.rp_extend import Controller
 
-def count_butter(plant: Plant, n:int = 1) -> AwaitableCondFunc:         #通过状态数黄油数量
-    def _cond_func(fm: FlowManager,v = VariablePool( projs = 0, try_to_shoot_time=None )):
+def count_butter(plant: Plant, n:int = 1, non_stop: bool = True, continuous: bool = False) -> AwaitableCondFunc:         #通过状态数黄油数量
+    def _cond_func(fm: FlowManager,v = VariablePool(projs = 0, try_to_shoot_time=None)):
         if plant.generate_cd == 1:                                      # 下一帧开打
             v.try_to_shoot_time = fm.time + 1
         if v.try_to_shoot_time == fm.time :
             if plant.status is PlantStatus.kernelpult_launch_butter :
                 v.projs += 1
             elif plant.launch_cd == 0 :
-                v.projs = 0
+                if non_stop | continuous:
+                    v.projs = 0
+            else:
+                if continuous:
+                    v.projs = 0
         if v.projs == n:
             return True 
         return False
@@ -34,19 +38,21 @@ def fun(ctler: Controller):
         0  0  1200
         2-9 4-9 2-9''')
     
-    def count_butter2(plant: Plant, n:int = 1) -> AwaitableCondFunc:         #通过子弹数黄油数量
+    def count_butter2(plant: Plant, n:int = 1, non_stop: bool = True, continuous:bool = False) -> AwaitableCondFunc:         #通过子弹数黄油数量
         def _cond_func(fm: FlowManager,v = VariablePool( projs = 0, count_down = 0,try_to_shoot_time=None)):
             if plant.generate_cd == 1:                                      # 下一帧可能开打
                 v.try_to_shoot_time = fm.time + 1
             if (v.try_to_shoot_time == fm.time) and (plant.launch_cd == 0): #并没有攻击，计数重置
-                v.projs = 0
-                v.count_down = 0
+                if non_stop | continuous:
+                    v.projs = 0
             if (v.try_to_shoot_time == fm.time) and (plant.launch_cd != 0): #确定要攻击，把launch_cd传给count_down
                 v.count_down = plant.launch_cd
             if v.count_down == 1 :                                          #在count_down到1时，黄油才可以被获得。
                 for proj in ~iz_test.game_board.projectile_list:
                     if proj.type_ == ProjectileType.butter :
                         v.projs += 1
+                    elif continuous:
+                        v.projs = 0
             if v.projs == n:
                 return True 
             if v.count_down !=0:
@@ -56,12 +62,14 @@ def fun(ctler: Controller):
 
     @iz_test.flow_factory.add_flow()
     async def place_zombie(_):
-        # zb = iz_test.game_board.zombie_list[0]
-        pl = iz_test.ground["2-1"]
+        # zlist = iz_test.game_board.zombie_list
+        # zb = zlist[0]
+        plist = iz_test.ground
+        pl = plist["2-1"]
         await count_butter(pl,2)
         place("lz 3-6")
 
-    iz_test.start_test(jump_frame=False, speed_rate=2) 
+    iz_test.start_test(jump_frame=False, speed_rate=3) 
 
 with InjectedGame(r"D:\pvz\Plants vs. Zombies 1.0.0.1051 EN\PlantsVsZombies.exe") as game:
     fun(game.controller)
--- useful/plant_shoot_test.py
+++ useful/plant_shoot_test.py
@@ -6,15 +6,16 @@ from rpze.flow.utils import AwaitableCondFunc, VariablePool
 from rpze.flow.flow import FlowManager
 from rpze.structs.plant import Plant
 
-def until_plant_n_shoot(plant: Plant, n:int = 1) -> AwaitableCondFunc:
+def until_plant_n_shoot(plant: Plant, n:int = 1, non_stop :bool = True) -> AwaitableCondFunc:
     def _cond_func(fm: FlowManager,
                 v=VariablePool(try_to_shoot_time=None, shots = 0)):
         if plant.generate_cd == 1:  # 下一帧开打
             v.try_to_shoot_time = fm.time + 1
         if v.try_to_shoot_time == fm.time and plant.launch_cd != 0:  # 在攻击时
             v.shots += 1
-        if v.try_to_shoot_time == fm.time and plant.launch_cd == 0: #不再攻击时，计数清零
-            v.shots = 0
+        if non_stop:
+            if v.try_to_shoot_time == fm.time and plant.launch_cd == 0: #不再攻击时，计数清零
+                v.shots = 0
         if v.shots == n:
             return True
         return False
@@ -39,7 +40,7 @@ def fun(ctler: Controller):
         await until_plant_n_shoot(d,2).after(30)
         place("tz 2-6")
 
-    iz_test.start_test(jump_frame=1, speed_rate=1)
+    iz_test.start_test(jump_frame=0, speed_rate=2)
 
 with InjectedGame(r"D:\pvz\Plants vs. Zombies 1.0.0.1051 EN\PlantsVsZombies.exe") as game:
     fun(game.controller)
