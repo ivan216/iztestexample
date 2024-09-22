7ee88f626287caeef17ee2259c25471a12144af8
ivan216 <64449776+ivan216@users.noreply.github.com>
2024-09-22 14:15:06 +0800

    Add files via upload

--- jisi/mj3.py
+++ jisi/mj3.py
@@ -16,18 +16,19 @@ def fun(ctler: Controller):
         dpptz
         .....
         .....
-        .....
+        ...h.
         mj 
         0  
         1-9''')
     
     _75_count = 0
+    can_bu = False
     
     @iz_test.flow_factory.add_flow()
     async def place_zombie(_):
-        nonlocal _75_count
+        nonlocal _75_count,can_bu
+        can_bu = False
         mj = iz_test.game_board.zombie_list[0]
-        z = iz_test.ground["2-5"]
         wb_num = 0
 
         while wb_num < 4:
@@ -39,13 +40,27 @@ def fun(ctler: Controller):
                     await until(lambda _:wb.is_dead)
 
         if mj.x >= 256: #256
+            await until(lambda _:can_bu)
             place("cg 1-6")
             _75_count += 1
+
         elif mj.hp < 320: #有可能被打7豌豆
+            await until(lambda _:can_bu)
             place("cg 1-6") 
             _75_count += 1
 
-    iz_test.start_test(jump_frame=0, speed_rate=10)
+    @iz_test.flow_factory.add_flow()
+    async def place_zombie(_):
+        nonlocal can_bu
+        h = iz_test.ground["1-4"]
+        h2 = iz_test.ground["5-4"]
+        await until(lambda _:h.is_dead)
+        await until(lambda _:315 <= iz_test.game_board.mj_clock%460 <= 352).after(randint(0,10))
+        mj2 = place("mj 5-6")
+        await until(lambda _:h2.hp <= 236)
+        can_bu = True
+
+    iz_test.start_test(jump_frame=1, speed_rate=5)
     print(_75_count)
 
 with InjectedGame(r"D:\pvz\Plants vs. Zombies 1.0.0.1051 EN\PlantsVsZombies.exe") as game:
