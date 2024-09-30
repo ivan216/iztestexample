edbd768ab7e767144453098ffa80bf5d136bc842
ivan216 <64449776+ivan216@users.noreply.github.com>
2024-09-30 19:37:25 +0800

    Add files via upload

--- useful/count_butter.py
+++ useful/count_butter.py
@@ -3,27 +3,60 @@ from rpze.iztest.iztest import IzTest
 from rpze.iztest.operations import place
 from rpze.flow.flow import FlowManager
 from rpze.flow.utils import AwaitableCondFunc, VariablePool
-from rpze.structs.plant import Plant , PlantStatus
+from rpze.structs.plant import Plant, PlantStatus
 from rpze.structs.projectile import ProjectileType
 from rpze.rp_extend import Controller
 
-def count_butter(plant: Plant, n:int = 1, non_stop: bool = True, continuous: bool = False) -> AwaitableCondFunc[None]:         #通过状态数黄油数量
-    def _cond_func(fm: FlowManager,v = VariablePool(projs = 0, try_to_shoot_time=None)):
-        if plant.generate_cd == 1:                                      # 下一帧开打
+from typing import Literal
+
+CountButterModeLiteral = Literal[0, 1, 2, "total", "nonstop", "continuous"]
+"""数黄油函数的计数方法
+
+    - 0 或 "total" 表示允许攻击中断
+    - 1 或 "nonstop" 表示攻击不中断
+    - 2 或 "continuous" 表示攻击不中断, 而且黄油必须连续投出
+"""
+
+
+def until_n_butter(plant: Plant, n: int = 1, mode: CountButterModeLiteral = 1) -> AwaitableCondFunc[None]:
+    """
+    生成一个 等到玉米攻击n发黄油 的函数
+
+    Args:
+        plant: 要判断的植物
+        n: 攻击黄油次数
+        mode: 字面量, 表示计数方法。0 或 "total" 表示允许攻击中断; \
+            1 或 "nonstop" 表示攻击不中断; \
+            2 或 "continuous" 表示攻击不中断, 而且黄油必须连续投出
+    """
+    match mode:
+        case "total" | 0:
+            mode_index = 0
+        case "nonstop" | 1:
+            mode_index = 1
+        case "continuous" | 2:
+            mode_index = 2
+        case _:
+            raise ValueError(" invalid mode !")
+
+    def _await_func(fm: FlowManager, v=VariablePool(projs=0, try_to_shoot_time=None)):
+        if plant.generate_cd == 1:  # 下一帧开打
             v.try_to_shoot_time = fm.time + 1
-        if v.try_to_shoot_time == fm.time :
-            if plant.status is PlantStatus.kernelpult_launch_butter :
+        if v.try_to_shoot_time == fm.time:
+            if plant.status is PlantStatus.kernelpult_launch_butter:  # 出黄油
                 v.projs += 1
-            elif plant.launch_cd == 0 :
-                if non_stop or continuous:
+            elif plant.launch_cd == 0:  # 攻击停止
+                if mode_index != 0:
                     v.projs = 0
-            else:
-                if continuous:
+            else:  # 出玉米粒
+                if mode_index == 2:
                     v.projs = 0
         if v.projs == n:
             return True 
         return False
-    return AwaitableCondFunc(_cond_func)
+    
+    return AwaitableCondFunc(_await_func)
+
 
 def fun(ctler: Controller):
     iz_test = IzTest(ctler).init_by_str('''
@@ -38,12 +71,13 @@ def fun(ctler: Controller):
         0  0  1200
         2-9 4-9 2-9''')
     
-    def count_butter2(plant: Plant, n:int = 1, non_stop: bool = True, continuous:bool = False) -> AwaitableCondFunc[None]:         #通过子弹数黄油数量
+    def until_n_butter2(plant: Plant, n:int = 1, mode:int = 1) -> AwaitableCondFunc[None]: #通过子弹数黄油数量
+        "mode = 0 or 1 or 2"
         def _cond_func(fm: FlowManager,v = VariablePool( projs = 0, count_down = 0,try_to_shoot_time=None)):
             if plant.generate_cd == 1:                                      # 下一帧可能开打
                 v.try_to_shoot_time = fm.time + 1
             if (v.try_to_shoot_time == fm.time) and (plant.launch_cd == 0): #并没有攻击，计数重置
-                if non_stop or continuous:
+                if mode != 0 :
                     v.projs = 0
             if (v.try_to_shoot_time == fm.time) and (plant.launch_cd != 0): #确定要攻击，把launch_cd传给count_down
                 v.count_down = plant.launch_cd
@@ -51,7 +85,7 @@ def fun(ctler: Controller):
                 for proj in ~iz_test.game_board.projectile_list:
                     if proj.type_ == ProjectileType.butter :
                         v.projs += 1
-                    elif continuous:
+                    elif mode == 2:
                         v.projs = 0
             if v.projs == n:
                 return True 
@@ -66,10 +100,10 @@ def fun(ctler: Controller):
         # zb = zlist[0]
         plist = iz_test.ground
         pl = plist["2-1"]
-        await count_butter(pl,2)
+        await until_n_butter(pl,2,1)
         place("lz 3-6")
 
-    iz_test.start_test(jump_frame=False, speed_rate=3) 
+    iz_test.start_test(jump_frame=0, speed_rate=3) 
 
 with InjectedGame(r"D:\pvz\Plants vs. Zombies 1.0.0.1051 EN\PlantsVsZombies.exe") as game:
     fun(game.controller)
