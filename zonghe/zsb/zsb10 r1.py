48d6cc48b49e801062688a12068736ea1e7e3e12
ivan216 <64449776+ivan216@users.noreply.github.com>
2024-10-11 17:52:37 +0800

    Add files via upload

--- zonghe/zsb/zsb10 r1.py
+++ zonghe/zsb/zsb10 r1.py
@@ -3,27 +3,7 @@ from rpze.iztest.iztest import IzTest
 from rpze.flow.utils import until, delay
 from rpze.iztest.operations import place ,repeat
 from rpze.rp_extend import Controller
-from rpze.flow.flow import FlowManager
-from rpze.flow.utils import AwaitableCondFunc, VariablePool
-from rpze.structs.plant import Plant , PlantStatus
-
-def count_butter(plant: Plant, n:int = 1, non_stop: bool = True, continuous: bool = False) -> AwaitableCondFunc[None]:         #通过状态数黄油数量
-    def _cond_func(fm: FlowManager,v = VariablePool(projs = 0, try_to_shoot_time=None)):
-        if plant.generate_cd == 1:                                      # 下一帧开打
-            v.try_to_shoot_time = fm.time + 1
-        if v.try_to_shoot_time == fm.time :
-            if plant.status is PlantStatus.kernelpult_launch_butter :
-                v.projs += 1
-            elif plant.launch_cd == 0 :
-                if non_stop or continuous:
-                    v.projs = 0
-            else:
-                if continuous:
-                    v.projs = 0
-        if v.projs == n:
-            return True 
-        return False
-    return AwaitableCondFunc(_cond_func)
+from rpze.iztest.cond_funcs import until_n_butter
 
 def fun(ctler: Controller):
     iz_test = IzTest(ctler).init_by_str('''
@@ -50,7 +30,7 @@ def fun(ctler: Controller):
         c = plist["2-3"]
         y = plist["1-1"]
         
-        await (until(lambda _:h.is_dead or tt.hp <90) | count_butter(y,2).after(142))
+        await (until(lambda _:h.is_dead or tt.hp <90) | until_n_butter(y,2).after(142))
         if h.is_dead:
             await until(lambda _:tt.hp <= 110 or tt.int_x <= 330)
             cg = place("cg 1-6")
