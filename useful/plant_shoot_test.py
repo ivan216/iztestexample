49bff0d4ea796eb21c193c219778c64af2c484ca
ivan216 <64449776+ivan216@users.noreply.github.com>
2024-10-11 16:05:36 +0800

    Add files via upload

--- useful/plant_shoot_test.py
+++ useful/plant_shoot_test.py
@@ -4,22 +4,35 @@ from rpze.rp_extend import Controller
 from rpze.iztest.operations import place
 from rpze.flow.utils import AwaitableCondFunc, VariablePool
 from rpze.flow.flow import FlowManager
-from rpze.structs.plant import Plant
+from rpze.structs.plant import Plant, PlantType
 
-def until_plant_n_shoot(plant: Plant, n:int = 1, non_stop :bool = True) -> AwaitableCondFunc:
-    def _cond_func(fm: FlowManager,
-                v=VariablePool(try_to_shoot_time=None, shots = 0)):
-        if plant.generate_cd == 1:  # 下一帧开打
+def until_plant_n_shoot(plant: Plant, n: int = 1, non_stop: bool = True) -> AwaitableCondFunc[None]:
+    """
+    生成一个 等到植物n次攻击 的函数
+
+    Args:
+        plant: 要判断的植物
+        n: 攻击次数
+        non_stop: 是否是不间断攻击
+    """
+
+    shoot_next_gcd = 1 if plant.type_ is not PlantType.split_pea else 26  # 修正裂荚攻击时机
+    
+    def _await_func(fm: FlowManager,
+                    v=VariablePool(try_to_shoot_time=None, shots=0)):
+        if plant.generate_cd == shoot_next_gcd:  # 下一帧开打
             v.try_to_shoot_time = fm.time + 1
-        if v.try_to_shoot_time == fm.time and plant.launch_cd != 0:  # 在攻击时
-            v.shots += 1
-        if v.try_to_shoot_time == fm.time and plant.launch_cd == 0: #不再攻击时，计数清零
-            if non_stop:
-                v.shots = 0
+        if v.try_to_shoot_time == fm.time:
+            if plant.launch_cd > 15:  # 在攻击时
+                v.shots += 1
+            else:  # 不再攻击时
+                if non_stop:  # 设置了不停止标志，则计数清零
+                    v.shots = 0
         if v.shots == n:
             return True
         return False
-    return AwaitableCondFunc(_cond_func)
+    
+    return AwaitableCondFunc(_await_func)
 
 def fun(ctler: Controller):
     iz_test = IzTest(ctler).init_by_str('''
