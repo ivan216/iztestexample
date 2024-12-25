09396fc510b982ca629aa15be95a8c244cbe0944
ivan216 <644767800@qq.com>
2024-12-25 11:46:57 +0800

    Update zdtoz.py

--- useful/zdtoz.py
+++ useful/zdtoz.py
@@ -24,7 +24,7 @@ def fun(ctler: Controller):
     d_die = 0
 
     @iz_test.flow_factory.add_flow()
-    async def place_zombie(_):
+    async def _(_):
         nonlocal dz_eat
         d = iz_test.ground["1-2"]
         z = iz_test.ground["1-5"]
@@ -37,15 +37,19 @@ def fun(ctler: Controller):
         await until(lambda _: z2.status is PlantStatus.chomper_chew)
         dz_eat += 1
 
-    @iz_test.flow_factory.add_tick_runner()
-    def check_end(fm:FlowManager):
+    @iz_test.on_game_end()
+    def _(res:bool):
         nonlocal d_die
+        if not res:
+            if iz_test.ground["1-2"] is None:
+                d_die += 1
+
+    @iz_test.flow_factory.add_tick_runner()
+    def _(fm:FlowManager):
         if iz_test.ground["1-0"] is None:
             return iz_test.end(True)
         if fm.time > 1000:
             if iz_test.game_board.zombie_list.obj_num == 0:
-                if iz_test.ground["1-2"] is None:
-                    d_die += 1
                 return iz_test.end(False)
 
     iz_test.start_test(jump_frame=1, speed_rate=3)
